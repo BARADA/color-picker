81aab501da4b1be51269db1a70f04b9a9e9b0ad0
BARADA <barada199222@gmail.com>
2021-06-07 22:35:47 +0300

    Initial App, install libraries: classnames, react-color, sass, tinycolor2.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Color Picker.
+				</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
